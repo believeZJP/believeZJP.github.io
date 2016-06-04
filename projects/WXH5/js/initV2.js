aa1c47f62755e68e74b3604bbbbd051fb76b7b3d
believezjp <1811886228@qq.com>
2016-06-04 11:01:27 +0800

    delete unused cod

--- projects/WXH5/js/initV2.js
+++ projects/WXH5/js/initV2.js
@@ -33,35 +33,16 @@
 				//以下显示遮罩层
 				var winHeight2 = document.documentElement.clientHeight;
 				var winHeight = window.screen.height;
-				
 				winHeight = winHeight2;
-				
-//				var winHeight = "";
-//				document.getElementsByClassName("islider-pic")[0].scrollHeight;
-//				var str ="<br/>li的高度"+ winHeight+"screenHeight"+winHeight+"__clientHeight:"+winHeight2+""+"innerHeight"+window.innerHeight;
 				var flag = false;
 				var gap = 0;
-		/*		if(winHeight>winHeight2){
-					flag = true;
-					gap = winHeight - winHeight2;
-					winHeight = winHeight2;
-				}*/
-//				document.getElementById("msg").textContent  = "差距"+gap+str;
-				console.log(winHeight);
-//				console.log(winHeight +":"+winHeight2);
 				var mask = document.getElementsByClassName("mask")[0];
 				mask.style.height = winHeight + "px";
 				mask.style.display = "block";
 				//判断屏幕和实际图片的缩放比
 				var rate = parseInt((winHeight/1334)*800);
-				
-				if(flag){
-					rate += gap;
-				}
 				rate = parseInt(rate)+"px";
 				mask.getElementsByClassName("guidArea")[0].style.top = rate;
-				
-				
 				window.setTimeout(function() {
 					S.unhold();//可以拖动
 					//删除遮罩层
@@ -69,14 +50,10 @@
 					//添加手势指引
 					var guide = document.getElementsByClassName("guideGesture")[0];
 					var rate2 = (winHeight/1334)*982;
-					if(flag){
-						rate2 += gap;
-					}
-					
 					guide.style.top = parseInt(rate2)+"px";
 					guide.style.display = "block";
 				}, 2000);
-			}, 500);
+			}, 2000);
 		}
 		S.on('slide', slideCallBack);
 		function slideCallBack(num, dom) {
