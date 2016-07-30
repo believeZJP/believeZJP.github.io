6cf27fae53050383c164f04c6dc4604d28fa0322
believezjp <1811886228@qq.com>
2016-07-30 21:10:45 +0800

    add webpack test

--- views/fe/webpackStart/app/index.js
+++ views/fe/webpackStart/app/index.js
@@ -4,7 +4,7 @@
 //引用sass
 require("./main.scss");
 
-
+//https://vikingmute.gitbooks.io/webpack-for-fools/content/entries/chapter-1.html
 var sub = require('./sub');
 var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World</h1>';
