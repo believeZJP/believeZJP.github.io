133d7f895d2355e6d57e9afbc0c30072b053292e
believezjp <1811886228@qq.com>
2017-05-12 18:08:43 +0800

    add __proto__

--- js/es62.js
+++ js/es62.js
@@ -951,16 +951,58 @@ Reflect.ownKeys({ [Symbol()]:0, b:0, 10:0, 2:0, a:0 })//["2", "10", "b", "a", Sy
 /**
  * 8.__proto__属性，Object.setPrototypeOf(),Object.getPrototypeOf()
  * 
- * 
+ * __proto__属性（前后各两个下划线），用来读取或设置当前对象的prototype对象。,建议不要使用
+ * 用Object.setPrototypeOf()（写操作）、Object.getPrototypeOf()（读操作）、Object.create()（生成操作）代替。
  */
 
+//es5
+var obj = Object.create(someOtherObj);
+obj.method = function(){...}
 
+//es6
+var obj = {
+	method: function(){...}
+}
+obj.__proto__ = someOtherObj;
 
+//__proto__实现原理：调用Object.prototype.__proto__
+Object.defineProperty(Object.prototype, '__proto__', {
+	get() {
+		let _thisObj = Object(this);
+		return Object.getPrototypeOf(_thisObj);
+	},
+	set(proto){
+		if(this === undefined || this === null){
+			throw new TypeError();
+		}
+		if(!isObject(this)){
+			return undefined;
+		}
+		if(!isObject(proto)){
+			return undefined;
+		}
+		let status = Reflect.setPrototypeOf(this, proto);
+		if(!status){
+			throw new TypeError();
+		}
+	}
+});
 
+function isObject(value){
+	return Object(value) === value;
+}
+//如果一个对象本身部署了__proto__属性，则该属性的值就是对象的原型。
+Object.getPrototypeOf({ __proto__: null })
 
-
-
-
+//语法
+Object.setPrototypeOf(object, prototype)
+//用法
+var o = Object.setPrototypeOf({}, null);
+//等同于
+function(obj, proto){
+	obj.__proto__ = proto;
+	return obj;
+}
 
 
 
